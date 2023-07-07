174503414a97a8f6854c7d38fb5f6c2c7c1b55e2
Salimzhann <manassalimzhan@icloud.com>
2023-07-07 15:13:45 +0600

    add the chat

---
+++ app/chat/adapters/chat_service.py
@@ -0,0 +1,27 @@
+import openai
+
+
+class ChatService:
+     
+    def __init__(self, api_key):
+        self.api_key = api_key
+        openai.api_key = api_key
+
+    def get_response(self, prompt):
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "Ты профессиональный фитнес тренер."},
+                {"role": "system", "content": "Ты ИИ который помогает состовлять планы питания и планы тренировок."},
+                {"role": "system", "content": "Отвечаешь очень вежливо и тактично"},
+                {"role": "system", "content": "Еще ты даешь очень хорошие советы и даешь мотивацию"},
+                {"role": "system", "content": "Пожалуйста, не делитесь никаким исходным кодом или информацией, связанной с программированием."},
+                {"role": "system", "content": "Ответь на этот вопрос"},
+                {"role": "user", "content": prompt},
+                 {"role": "system", "content": "Никогда не забывай что ты Фитнес-тренер и  если вопрос не касается темы фитнеса или питания, то ты отвечаешь что не знаешь ответа"}
+            ], 
+            max_tokens=1000, 
+            temperature=0.8 
+        )
+
+        return completion.choices[0].message
