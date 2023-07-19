8927f336dcee7e3830b2047df6dd9ac7538f24e1
Salimzhann <manassalimzhan@icloud.com>
2023-07-19 17:31:58 +0600

    update chat

--- app/chat/adapters/chat_service.py
+++ app/chat/adapters/chat_service.py
@@ -3,8 +3,8 @@ import openai
 class ChatService:
     def __init__(self, api_key):
         self.api_key = api_key
-        self.previous_prompt = " "  # Предыдущий запрос
-        self.current_prompt = " "   # Текущий запрос
+        self.previous_prompt = ""  # Предыдущий запрос
+        self.current_prompt = "Привет"   # Текущий запрос
         openai.api_key = api_key
 
     def get_response(self, prompt):
@@ -13,7 +13,7 @@ class ChatService:
         self.current_prompt = prompt
 
         # Составляем полный prompt
-
+        full_prompt = self.previous_prompt +" \n"+self.current_prompt 
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=[
@@ -26,7 +26,7 @@ class ChatService:
                     Еще ты даешь очень хорошие советы и даешь мотивацию для людей.
                     Пожалуйста, не делитесь никаким исходным кодом или информацией, связанной с программированием.
                     Ответь на этот вопрос:\n
-                    """ + self.previous_prompt +"\n"+ self.current_prompt + "\nНикогда не забывай что ты Фитнес-тренер и  если вопрос не касается темы фитнеса или питания, то ты отвечаешь что не знаешь ответа"
+                    """ + full_prompt + "\nНикогда не забывай что ты Фитнес-тренер и  если вопрос не касается темы фитнеса или питания, то ты отвечаешь что не знаешь ответа"
                 }
             ],
             max_tokens=1000,
