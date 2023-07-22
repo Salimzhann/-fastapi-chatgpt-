2efb6c7845a35aa55f2ea0947d1436663afe429b
Salimzhann <manassalimzhan@icloud.com>
2023-07-22 19:03:42 +0600

    changes

--- app/chat/adapters/chat_service.py
+++ app/chat/adapters/chat_service.py
@@ -1,21 +1,14 @@
-import openai
-from langchain.memory import ChatMessageHistory
-
-
 class ChatService:
      
     def __init__(self, api_key):
         self.api_key = api_key
         openai.api_key = api_key
-        self.history = ChatMessageHistory()
 
     def get_response(self, prompt):
-        self.history.add_user_message(prompt)
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
             messages=[
                 {"role": "system", "content": """
-
                  Ты - профессиональный фитнес-тренер и ИИ-ассистент, специализирующийся в планировании персонализированных питания и тренировок. Твоя цель - помочь людям достичь оптимальной формы, здоровья и уровня физической активности.
 В твои обязанности входит:
 - Составление индивидуальных планов питания.
@@ -26,12 +19,9 @@ class ChatService:
 Важно заметить, что ты не можешь отвечать на вопросы, не касающиеся фитнеса, здоровья или медицины. Если получишь запрос, который выходит за рамки твоей компетенции, вежливо уведоми клиента, что не можешь на него ответить.
 
                  Ответь на этот вопрос:\n
-                 """ + self.history.get_message_string() + "Никогда не забывай что ты Фитнес-тренер и  если вопрос не касается темы фитнеса или питания, то ты отвечаешь что не знаешь ответа"},
+                 """ + prompt + "Никогда не забывай что ты Фитнес-тренер и  если вопрос не касается темы фитнеса или питания, то ты отвечаешь что не знаешь ответа"},
             ], 
             max_tokens=1000, 
             temperature=0.8 
         )
-        assistant_response = completion.choices[0].message["content"]
-        self.history.add_ai_message(assistant_response)
-        
-        return assistant_response
+        return completion.choices[0].message
