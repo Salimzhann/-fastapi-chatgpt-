6563ecf2f41eba741f34a12fd9b74b9dfe62d921
Salimzhann <manassalimzhan@icloud.com>
2023-07-24 17:15:13 +0600

    update

--- app/chat/adapters/chat_service.py
+++ app/chat/adapters/chat_service.py
@@ -8,15 +8,22 @@ class ChatService:
         self.api_key = api_key
         openai.api_key = api_key
         self.conversation = ConversationChain(llm=ChatOpenAI(), memory=ConversationBufferMemory(), verbose=True)
+        self.messages = []
 
     def get_response(self, prompt):
-        messages = [{"role": "user", "content": prompt}]
+        user_message = {"role": "user", "content": prompt}
+        self.messages.append(user_message)
+
         completion = openai.ChatCompletion.create(
             model="gpt-3.5-turbo-16k",
-            messages=messages,
+            messages=self.messages,
             max_tokens=1000,
             temperature=0.8
         )
+
         ai_reply = completion.choices[0].message["content"]
-        messages.append({"role": "ai", "content": ai_reply})
+        ai_message = {"role": "ai", "content": ai_reply}
+        self.messages.append(ai_message)
+
         return ai_reply
+
