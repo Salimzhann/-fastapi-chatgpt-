b4a655f5be0b48172e805ae89ccaa15d391b6e6a
Salimzhann <manassalimzhan@icloud.com>
2023-07-24 19:26:24 +0600

    lol

--- app/chat/adapters/chat_service.py
+++ app/chat/adapters/chat_service.py
@@ -5,28 +5,29 @@ class ChatService:
     def __init__(self, api_key):
         self.api_key = api_key
         openai.api_key = api_key
-        self.questions = []
         self.messages = []
+        self.last_user_request = None
+        self.last_ai_response = None
 
     def get_response(self, prompt):
-        # self.messages.append({"role": "user", "content": prompt})
-        if len(self.questions) == 0:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-16k",
-                messages=f'{self.questions}, {self.messages}. question: {prompt}',
-                max_tokens=1000, 
-                temperature=0.8 
-            )
-        else:
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo-16k",
-                messages=f'last question: {self.questions[-1]} last answer: {self.messages[-1]}. current question: {prompt}',
-                max_tokens=1000, 
-                temperature=0.8 
-            )
+        if self.last_user_request is not None:
+            self.messages.append({"role": "user", "content": self.last_user_request})
+        if self.last_ai_response is not None:
+            self.messages.append({"role": "system", "content": self.last_ai_response})
 
-        self.questions.append(prompt)
-        response = completion['choices'][0]['message']['content']
-        self.messages.append(response)
+        self.messages.append({"role": "user", "content": prompt})
+        
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=self.messages,
+            max_tokens=1000, 
+            temperature=0.8 
+        )
+        
+        response = completion.choices[0].message
+        self.messages.append({"role": "system", "content": response})
+
+        self.last_user_request = prompt
+        self.last_ai_response = response
         
         return response
