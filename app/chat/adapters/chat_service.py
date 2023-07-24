54e95ef027c6e7212716ae32c3315da6aec4b306
Salimzhann <manassalimzhan@icloud.com>
2023-07-24 18:46:21 +0600

    lol

--- app/chat/adapters/chat_service.py
+++ app/chat/adapters/chat_service.py
@@ -5,19 +5,28 @@ class ChatService:
     def __init__(self, api_key):
         self.api_key = api_key
         openai.api_key = api_key
+        self.questions = []
         self.messages = []
 
     def get_response(self, prompt):
-        self.messages.append({"role": "user", "content": prompt})
-        
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
-            messages=self.messages,
-            max_tokens=1000, 
-            temperature=0.8 
-        )
-        
+        # self.messages.append({"role": "user", "content": prompt})
+        if len(self.questions) == 0:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                messages=prompt,
+                max_tokens=1000, 
+                temperature=0.8 
+            )
+        else:
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo-16k",
+                messages=f'last question: {self.questions[-1]} last answer: {self.messages[-1]}. current question: {prompt}',
+                max_tokens=1000, 
+                temperature=0.8 
+            )
+
+        self.questions.append(prompt)
         response = completion['choices'][0]['message']['content']
-        self.messages.append({"role": "system", "content": response})
+        self.messages.append(response)
         
         return response
