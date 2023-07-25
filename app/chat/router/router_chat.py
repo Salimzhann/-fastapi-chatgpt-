441b0d21b6cafa317e562e9ec4e14499731f5f31
Salimzhann <manassalimzhan@icloud.com>
2023-07-25 14:55:35 +0600

    upd

--- app/chat/router/router_chat.py
+++ app/chat/router/router_chat.py
@@ -16,12 +16,11 @@ class ChatResponse(AppModel):
     response: str
 
 
-@router.post("/")
+@router.post("/", response_model=ChatResponse)
 def chat_with_ai(
     request: ChatRequest,
     svc: Service = Depends(get_service),
-) -> str:
+) -> ChatResponse:
     prompt = request.prompt
     response = svc.chat_service.get_response(prompt)
-    print(response)
-    return response["content"]
+    return ChatResponse(response=response)
